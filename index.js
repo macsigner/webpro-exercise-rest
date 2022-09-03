4f99aac4fc7c565002c1d494dda8cfcf2377f1f8
Mac Signer <macsigner@gmx.ch>
2022-09-03 10:20:13 +0200

    Add current time route

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('start')
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/current-time', (req, res) => {
+    let date = new Date();
+    res.send(`${date.getHours()}:${date.getMinutes()}`);
+});
+
+app.listen(port, () => {
+    console.log(`Start listen at http://localhost:${port}`);
+})
